9d10a851bdca2e43edf63f1aa6963558a3dcf90c
JoaquinManz <altairyezio2087@gmail.com>
2023-12-04 12:06:58 -0300

    final

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from './components/NavBar.jsx'
+import Hero from "./components/Hero.jsx";
+import Analytics from "./components/Analytics.jsx";
+import Newsletter from "./components/Newsletter.jsx";
+import Cards from "./components/Cards.jsx";
+import Footer from "./components/Footer.jsx";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
